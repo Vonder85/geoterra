f2dfb56e671eb7b7f607cf35ce0df9b2a50a49ae
Vonder <julienschiano@gmail.com>
2025-10-03 21:18:21 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: "hybrid",
   adapter: vercel(),
-  integrations: [
-    tailwind(),
-    sitemap()
-  ],
-  site: 'https://geoterra.fr',
+  integrations: [tailwind(), sitemap()],
+  site: "https://geoterra.fr",
   compilerOptions: {
-    strict: true
-  }
+    strict: true,
+  },
+  adapter: vercel({
+    runtime: "nodejs20.x",
+  }),
 });
