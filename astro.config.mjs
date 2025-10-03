bfec6c7850a2975b48689e808b4d12c4ba87dbaa
Vonder <julienschiano@gmail.com>
2025-10-03 21:13:21 +0200

    Fix: Switch to Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [
     tailwind(),
     sitemap()
