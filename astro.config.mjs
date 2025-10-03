075e9f94fa9118cd96a8e0c82d56136605f32f18
Vonder <julienschiano@gmail.com>
2025-10-03 21:36:51 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "server",
   site: "https://geoterra.fr",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   adapter: vercel({
     runtime: "nodejs20.x",
   }),
