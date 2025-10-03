336300fc0c2b7f715cd031a04de00331cc2dcec8
Vonder <julienschiano@gmail.com>
2025-10-03 17:06:42 +0200

    form ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [
     tailwind(),
     sitemap()
